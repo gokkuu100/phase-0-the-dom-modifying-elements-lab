c73d5e43fc0dcf901bd132a2cd2b91df6ca05d42
goku <wkurts247@gmail.com>
2023-06-22 17:28:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+//add text content to div element
+let textNode = document.createTextNode("Prince is the champion");
+
+newHeader.appendChild(textNode);
+
+const element = document.getElementById("this");
